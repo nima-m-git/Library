7e374a26400d88853d2235039aee8a5d63b8d1b0
nima.mirzaei@hotmail.com <nima.mirzaei@hotmail.com>
2020-05-26 11:19:56 -0400

    restructured library to module, ID generator, removed globals, localStorage implemented

--- script.js
+++ script.js
@@ -1,163 +1,173 @@
-let myLibrary = [];
+const library = (function() {
+    const getLibrary = () => Object.values(localStorage).map(book => JSON.parse(book));
+    const getSortedLibrary = () => library.getLibrary().sort((a, b) => (a.id > b.id)? 1 : -1);
+    const getBook = (bookID) => JSON.parse(localStorage.getItem(bookID));
+    const addBook = (newBook) => localStorage.setItem(newBook.id, JSON.stringify(newBook));
+    const removeBook = (ID) => {
+        localStorage.removeItem(ID)
+        updateLibraryTable();
+    }
+    const readBook = (ID) => {
+        const book = getBook(ID);
+        book.read = (book.read == "yes") ? "no" : "yes";
+        library.addBook(book)
+        updateLibraryTable();
+    }
 
-let body = document.querySelector('body');
-let table = document.querySelector('table');
-
-let id = 0;
-const generateID = () => id++;
+    return {
+        getLibrary,
+        getSortedLibrary,
+        getBook,
+        addBook,
+        removeBook,
+        readBook,
+    }
+})();
 
 
 //          Book Constructor            \\
-class Book {
-    constructor(title, author, pages, read){
-        this.title = title
-        this.author = author
-        this.pages = pages
-        this.read = (read)? 'yes' : 'no';
-        this.id = generateID();
+const generateID = (function () {
+    let id = 0;
+    function newID() {
+        return id++;
     }
+    return {newID}
+})();
+
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read ? "yes" : "no";
+    this.id = generateID.newID();
+  }
 }
 
-Book.prototype.info = function() {
-    let haveRead = (this.read == 'true')? 'read' : 'not read yet';
-    return this.title + ' by ' + this.author + ', ' + this.pages + ' pages long, ' + haveRead
-}
-
+Book.prototype.info = function () {
+  let haveRead = this.read == "true" ? "read" : "not read yet";
+  return (this.title + " by " + this.author + ", " + this.pages + " pages long, " + haveRead)
+};
 
 //          Sample Books            \\
-myLibrary.push(
-    new Book('the cat in the hat', 'dr. suess', '23', true),
-    new Book('the lorax', 'dr. suess', 36, true),
-    new Book('green eggs and ham', 'dr. suess', '36', true),
-    new Book('a brave new world', 'aldous huxley', '343', true),
-    new Book('a clockwork orange', 'anthony burgess', '176', false),
-    new Book('moby dick', 'herman melville', 585, false),
-    new Book('python crash course', 'eric mattes', 652, true),
-    new Book('Organic Chemistry', 'L.G wade jr', 1326, true),
-    new Book('genetic analysis', 'griffiths et. al', 862, true),
-    new Book('principles of biochemistry', 'lehninger', 1119, true),
-    new Book('statistical thermodynamics and kinetics', 'thomas engel', 653, true),
-    );
+const sampleBooks = [
+  new Book("the cat in the hat", "dr. suess", "23", true),
+  new Book("the lorax", "dr. suess", 36, true),
+  new Book("green eggs and ham", "dr. suess", "36", true),
+  new Book("a brave new world", "aldous huxley", "343", true),
+  new Book("a clockwork orange", "anthony burgess", "176", false),
+  new Book("moby dick", "herman melville", 585, false),
+  new Book("python crash course", "eric mattes", 652, true),
+  new Book("Organic Chemistry", "L.G wade jr", 1326, true),
+  new Book("genetic analysis", "griffiths et. al", 862, true),
+  new Book("principles of biochemistry", "lehninger", 1119, true),
+  new Book("statistical thermodynamics and kinetics", "thomas engel", 653, true)
+];
+sampleBooks.map(book => library.addBook(book));
 
 
 //           User Add Book           \\
-function addBookToLibrary() {
-    const title = document.getElementById('title');
-    const author = document.getElementById('author');
-    const pages = document.getElementById('pages');
-    const read = document.getElementById('read');
-    
+document.getElementById('add-new-book').addEventListener('click', function() {
+    const title = document.getElementById("title");
+    const author = document.getElementById("author");
+    const pages = document.getElementById("pages");
+    const read = document.getElementById("read");
     const checkField = (field) => {
-        const valueMissing = field.validity.valueMissing;
-        if (valueMissing) {
-            field.setCustomValidity('fill out the field');
-        } else {
-            field.setCustomValidity('');
-        }
-        return !(valueMissing)
-    }
-
+      const valueMissing = field.validity.valueMissing;
+      if (valueMissing) {
+        field.setCustomValidity("fill out the field");
+      } else {
+        field.setCustomValidity("");
+      }
+      return !valueMissing;
+    };
+  
+    // form validation
     if ([title, author, pages].every((field) => checkField(field))) {
-        myLibrary.push(new Book(title.value, author.value, pages.value, read.checked));
-        updateLibrary();
-        resetForm();
+      const newBook = new Book(title.value, author.value, pages.value, read.checked);
+      library.addBook(newBook);
+      updateLibraryTable();
+      resetForm();
     }
+})
+
+function resetForm() {
+  const formInputs = document.querySelectorAll("input");
+  formInputs.forEach((input) => (input.value = ""));
 }
-
-
-function resetForm(){
-    const formInputs = document.querySelectorAll('input');
-    formInputs.forEach(input => input.value = '');
-}
-
 
 //         Create Table            \\
+let table = document.querySelector("table");
+
 function generateTableHead() {
-    const headers = Object.keys(myLibrary[0]);
-    headers.push('Remove', 'Toggle');
-    headers.splice(headers.indexOf('id'),1);
+  const headers = Object.keys(library.getLibrary()[0]);
+  headers.push("Remove", "Toggle");
+  headers.splice(headers.indexOf("id"), 1);
 
-    let thead = table.createTHead();
-    let row = thead.insertRow();
-    for (let header of headers) {
-        let th = document.createElement('th');
-        let text = document.createTextNode(header);
-        th.appendChild(text);
-        row.appendChild(th);   
-    }
-
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let header of headers) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(header);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
 }
 
 function generateTable() {
-    for (book of myLibrary){
-        let row = table.insertRow();
-        for (let key in book) {
-            if (book.hasOwnProperty(key) && key != 'id') {
-                let cellText = document.createTextNode(book[key]);
-                row.insertCell().appendChild(cellText);
-            }
-        }
-        let removeButton = createButton(book, 'remove', 'Remove', removeBook);
-        row.insertCell().appendChild(removeButton);
+  for (book of library.getSortedLibrary()) {
+    let row = table.insertRow();
+    for (let key in book) {
+      if (book.hasOwnProperty(key) && key != "id") {
+        let cellText = document.createTextNode(book[key]);
+        row.insertCell().appendChild(cellText);
+      }
+    }
+    let removeButton = createButton(book, "remove", "Remove", library.removeBook);
+    row.insertCell().appendChild(removeButton);
 
-        let readButton = createButton(book, 'read', 'Read?', readBook);
-        row.insertCell().appendChild(readButton);
-    } 
+    let readButton = createButton(book, "read", "Read?", library.readBook);
+    row.insertCell().appendChild(readButton);
+  }
 }
 
-function updateLibrary() {
-    body.removeChild(table);
-    newTable = document.createElement('table');
-    body.appendChild(newTable);
+function updateLibraryTable() {
+  const body = document.querySelector("body");
+  body.removeChild(table);
+  table = document.createElement("table"); // remove redeclaration?
+  body.appendChild(table);
 
-    table = document.querySelector('table');
-
-    generateTable();
-    generateTableHead();
+  generateTable();
+  generateTableHead();
 }
 
 //          Initial         \\
-for (book of myLibrary) {
-    updateLibrary(book);
+for (book of library.getSortedLibrary()) {
+  updateLibraryTable(book);
 }
-
 resetForm();
 
-
 //           Buttons           \\
-
-function getIndex(book){
-    for (i=0; i<myLibrary.length; i++) {
-        if (book.id == myLibrary[i].id){
-            return i
-        } 
+// Obsolete? ID used now
+function getIndex(book) {
+  for (i = 0; i < library.getLibrary().length; i++) {
+    if (book.id == library.getLibrary()[i].id) {
+      return i;
     }
+  }
 }
 
 function createButton(book, className, text, func) {
-    let button = document.createElement('button');
-    let index = getIndex(book);
-    button.value = index;
-    button.classList = className;
-    button.textContent = text;
-    button.onclick = function() {func(index); };
-    return button
+  let button = document.createElement("button");
+//   button.value = index;
+  button.classList = className;
+  button.textContent = text;
+  button.onclick = function () {
+    func(book.id);
+  };
+  return button;
 }
-
-function removeBook(index) {
-    myLibrary.splice(index, 1);
-    updateLibrary();
-}
-
-function readBook(index) {
-    myLibrary[index].read = (myLibrary[index].read=='yes')? 'no' : 'yes';
-    updateLibrary();
-}
-
-
-
-
-
 
 
 
